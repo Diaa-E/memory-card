5061292771926efe053cde23463b9e1fdb4d3c58
Diaa-E <blarb21@yahoo.com>
2024-01-22 06:04:08 +0200

    Add score, Add gameover flag

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import { v4 as generateId } from 'uuid';
 
 function App() {
   
+  const [gameover, setgameover] = useState(false);
+  const [score, setScore] = useState(0);
   const [cardImages, setCardImages] = useState(() => {
 
     const array = []
@@ -33,16 +35,33 @@ function App() {
     return cardsArray;
   }
 
+  function getCardIndex(cardId, cardsArray)
+  {
+    return cardsArray.findIndex((image) => image.id === cardId);
+  }
+
   function handleCardClick(cardId)
   {
-    let newCardImages = Array.from(cardImages);
-    newCardImages = Array.from(markCard(cardId, newCardImages));
-    newCardImages = Array.from(shuffleCards(newCardImages));
+    if (gameover) return;
+
+    const cardIndex = getCardIndex(cardId, cardImages);
+    
+    if (cardImages[cardIndex].clicked)
+    {
+      setgameover(true);
+      return;
+    }
+    
+    setScore(score + 1);
+    let newCardImages = [...cardImages];
+    newCardImages[cardIndex].clicked = true
+    newCardImages = [...shuffleCards(newCardImages)];
     setCardImages(newCardImages);
   }
 
   return (
     <>
+      <h1>Score: {score}</h1>
       {
         cardImages.map(image => <Card key={image.id} id={image.id} imgSrc={image.imgSrc} imgText={image.imgText} onClick={handleCardClick}/>)
       }
