3dc432d0b50da5c4846c907847d63bec671f7503
Diaa-E <blarb21@yahoo.com>
2024-01-24 16:58:19 +0200

    Fit card size into screen

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card({id, imgSrc, onClick})
 {
     return ( 
         <div>
-            <img onClick={() => onClick(id)} src={imgSrc.src} alt="card's image" />
+            <img className="card" onClick={() => onClick(id)} src={imgSrc.src} alt="card's image" />
         </div>
     )
 }
