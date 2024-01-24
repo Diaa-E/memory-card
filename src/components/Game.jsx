e07bb29b3da2fd0b104194d17fb6f42b1cd94e0b
Diaa-E <blarb21@yahoo.com>
2024-01-24 16:13:38 +0200

    Display time in minutes and seconds

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -7,6 +7,9 @@ export default function Game({enabled, cards, winner, gameover, onCardClick})
     const [time, setTime] = useState(0);
     const [score, setScore] = useState(0);
 
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60
+
     useEffect(() => {
 
         if (gameover) return;
@@ -24,7 +27,7 @@ export default function Game({enabled, cards, winner, gameover, onCardClick})
 
     return (
         <div className="game-container">
-            <h2 className="time">{time}</h2>
+            <h2 className="time">{minutes < 10? "0" + minutes : minutes}:{seconds < 10? "0" + seconds : seconds}</h2>
             <h2 className="score">{score}</h2>
             {
                 cards.map(image => <Card key={image.id} id={image.id} imgSrc={image.imgSrc} imgText={image.imgText} onClick={onCardClick}/>)
