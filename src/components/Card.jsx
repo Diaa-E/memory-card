a6f48d873dd0af6ed9aaf9a8775501c04d465795
Diaa-E <blarb21@yahoo.com>
2024-01-24 16:47:00 +0200

    Remove unused props - remove card text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,11 @@
 import "../styles/Card.css";
 import { useState } from "react";
 
-export default function Card({id, imgSrc, imgText, clicked, onClick})
+export default function Card({id, imgSrc, onClick})
 {
     return ( 
         <div>
-            <img onClick={() => onClick(id)} src={imgSrc.src} alt={`${imgText}'s image`} />
-            <p>{imgText}</p>
+            <img onClick={() => onClick(id)} src={imgSrc.src} alt="card's image" />
         </div>
     )
 }
