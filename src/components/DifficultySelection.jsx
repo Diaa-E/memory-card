bec99e440e47de0e66335233514df0a6b16a3e55
Diaa-E <blarb21@yahoo.com>
2024-01-27 11:19:56 +0200

    Add title - Add info tabs

--- src/components/DifficultySelection.jsx
+++ src/components/DifficultySelection.jsx
@@ -5,10 +5,17 @@ export default function DifficultySelection({enabled, setDifficulty})
     if (!enabled) return <></>
 
     return (
-        <div className="difficulty-container">
-            <button onClick={() => setDifficulty(6)} className="play-button easy-button">Easy</button>
-            <button onClick={() => setDifficulty(12)} className="play-button">Medium</button>
-            <button onClick={() => setDifficulty(18)} className="play-button hard-button">Hard</button>
-        </div>
+        <>
+            <div className="title-container">
+                <button className='play-button about-button'>Help</button>
+                <h1 className="title">Memory Cards</h1>
+                <button className='play-button about-button'>About</button>
+            </div>
+            <div className="difficulty-container">
+                <button onClick={() => setDifficulty(6)} className="play-button easy-button">Easy</button>
+                <button onClick={() => setDifficulty(12)} className="play-button">Medium</button>
+                <button onClick={() => setDifficulty(18)} className="play-button hard-button">Hard</button>
+            </div>
+        </>
     )
 }
