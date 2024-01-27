176d07f92e1b28fd3c9433e96d9a969ca3f8c9c6
Diaa-E <blarb21@yahoo.com>
2024-01-27 22:17:31 +0200

    Create about tab

---
+++ src/components/AboutTab.jsx
@@ -0,0 +1,14 @@
+import "../styles/Tabs.css";
+
+export default function AboutTab({enabled, closeDialog})
+{
+    if (!enabled) return <></>
+
+    return (
+        <div className="tab">
+            <p>Memory Cards v1.0.0</p>
+            <p>Visit project's repo <a href="https://github.com/Diaa-E/memory-card">Memory Cards repo</a></p>
+            <p>Created by Diaa-E <a href="https://github.com/Diaa-E/">Diaa-E on Github</a></p>
+        </div>
+    )
+}
--- src/components/DifficultySelection.jsx
+++ src/components/DifficultySelection.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import "../styles/DifficultySelection.css";
+import AboutTab from "./AboutTab";
 
 export default function DifficultySelection({enabled, setDifficulty})
 {
+    const tabs = {
+        none: "none",
+        about: "about",
+        help: "help"
+    }
+    const [activeTab, setActiveTab] = useState(tabs.none);
+
     if (!enabled) return <></>
 
     return (
         <>
-            <div className="title-container">
+            <div className={`title-container ${activeTab === tabs.none? "" : "title-container-open"}`}>
                 <button className='play-button about-button'>Help</button>
-                <h1 className="title">Memory Cards</h1>
-                <button className='play-button about-button'>About</button>
+                <button
+                    onClick={() => setActiveTab(tabs.none)}
+                    className="play-button about-button title"
+                >
+                    {activeTab === tabs.none? "Memory Cards" : "Go Back"}
+                </button>
+                <button onClick={() => setActiveTab(tabs.about)} className='play-button about-button'>About</button>
             </div>
-            <div className="difficulty-container">
+            <AboutTab enabled={activeTab === tabs.about} closeDialog={() => setActiveTab(tabs.none)}/>
+            <div className={`difficulty-container ${activeTab === tabs.none? "" : "invisible"}`}>
                 <button onClick={() => setDifficulty(6)} className="play-button easy-button">Easy</button>
                 <button onClick={() => setDifficulty(12)} className="play-button">Medium</button>
                 <button onClick={() => setDifficulty(18)} className="play-button hard-button">Hard</button>
