e18b3461c29dc2127e1e293db75a0f3a33a6778b
Diaa-E <blarb21@yahoo.com>
2024-01-28 12:16:15 +0200

    Improve how to play tips

--- src/components/HelpTab.jsx
+++ src/components/HelpTab.jsx
@@ -8,8 +8,8 @@ export default function HelpTab({enabled})
         <div className="tab">
             <p>You receive a <span className="span-highlight">number of cards
             </span> based on the <span className="span-highlight">difficulty</span> you choose.</p>
-            <p>Try to click all cards <span className="span-highlight">only once</span>.</p>
-            <p>Each card click adds <span className="span-highlight">one point</span> to the score</p>
+            <p>Each time a card is <span className="span-highlight">clicked</span>, all the cards get <span className="span-highlight">shuffled</span> around.</p>
+            <p>Each card click adds <span className="span-highlight">one point</span> to the score.</p>
             <p>You <span className="span-danger">lose</span> the game by clicking a card <span className="span-danger">twice</span>.</p>
             <p>You <span className="span-valid">win</span> the game by clicking all cards <span className="span-valid">once</span>.</p>
         </div>
