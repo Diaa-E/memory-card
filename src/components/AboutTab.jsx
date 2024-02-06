2f2afe2f8af1a66310413c4de5d60d2472eb4821
Diaa-E <blarb21@yahoo.com>
2024-02-06 18:23:08 +0200

    Build v1.0.2

--- src/components/AboutTab.jsx
+++ src/components/AboutTab.jsx
@@ -6,7 +6,7 @@ export default function AboutTab({enabled, closeDialog})
 
     return (
         <div className="tab">
-            <p>Memory Cards v1.0.1</p>
+            <p>Memory Cards v1.0.2</p>
             <p>Visit project's repo <a href="https://github.com/Diaa-E/memory-card">Memory Cards repo</a></p>
             <p>Created by Diaa-E <a href="https://github.com/Diaa-E/">Diaa-E on Github</a></p>
         </div>
